1a5ff4b14eb979c3b8690bf301c9cc6adddd4c43
Maria <mkrous14@student.aau.dk>
2016-09-30 10:09:37 +0200

    dfds

--- figures/MATLAB_KODE/overbelaegning_AUH.m
+++ figures/MATLAB_KODE/overbelaegning_AUH.m
@@ -4,6 +4,7 @@ data=xlsread('overbelaegning_AUH.xlsx');
 %% Vælger kollonen med data (gennemsnitsværdier)
 data1=data(:,1);
 
+
 %% Laver string
 str = {'Alb 1.Afdeling Nord og Dronninglund';'Alb 2.Afdeling (NOT/Farsø)';'Alb 3.Afdeling (TV)';'Alb 4.Afdeling (AHØR/Hobro)';'Alb Akut- og Traumecenter';'Alb Børneområde';'Alb Endokrinologisk Område';'Alb Geriatrisk Område';'Alb Gyn.-obst. Område';'Alb Hjerte-lungekirurgisk afd';'Alb Hæmatologisk Område';'Alb Infektionsmedicinsk Område';'Alb Kardiologisk Område';'Alb Karkirurgisk Område';'Alb Kir Gastro. Område';'Alb Kæbekirurgisk område';'Alb Lungemed. Område';'Alb Mammakirurgisk Område';'Alb Med.gastroenterologisk Område';'Alb Neurokir. Område';'Alb Neurologisk Område';'Alb Nyremedicinsk område';'Alb Onkologisk Område';'Alb Ortopædkirurgisk Område';'Alb Plastikkirurgisk Område';'Alb Reumatologisk Område';'Alb Urologisk Område';'Alb Vuggeområde';'Alb Øjenområde';'Alb Øre-Næse-Halsområde';'Dro Medicinsk Område';'Hob AMA';'Hob Medicinsk område'};
 
@@ -13,7 +14,7 @@ s2 =[8,8]
 %% plotter figur
 figure;
 bar(data1)
-plot(S1,S2,'r')
+%plot(S1,S2,'r')
 set(gca, 'XTickLabel',str, 'XTick',1:numel(str))
 set(gca,'XTickLabelRotation',45)
 title('Gennemsnitlig overbelægning på Aalborg Universitetshospitals afdelinger')
