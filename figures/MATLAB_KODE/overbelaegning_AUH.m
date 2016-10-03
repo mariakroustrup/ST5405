78260d9619bfcf1d52a8650aadcf8eca0fa79fab
Maria <mkrous14@student.aau.dk>
2016-10-03 13:06:10 +0200

    fds

--- figures/MATLAB_KODE/overbelaegning_AUH.m
+++ figures/MATLAB_KODE/overbelaegning_AUH.m
@@ -9,8 +9,11 @@ data1=data(:,1);
 str = {'Alb 1.Afdeling Nord og Dronninglund';'Alb 2.Afdeling (NOT/Farsø)';'Alb 3.Afdeling (TV)';'Alb 4.Afdeling (AHØR/Hobro)';'Alb Akut- og Traumecenter';'Alb Børneområde';'Alb Endokrinologisk Område';'Alb Geriatrisk Område';'Alb Gyn.-obst. Område';'Alb Hjerte-lungekirurgisk afd';'Alb Hæmatologisk Område';'Alb Infektionsmedicinsk Område';'Alb Kardiologisk Område';'Alb Karkirurgisk Område';'Alb Kir Gastro. Område';'Alb Kæbekirurgisk område';'Alb Lungemed. Område';'Alb Mammakirurgisk Område';'Alb Med.gastroenterologisk Område';'Alb Neurokir. Område';'Alb Neurologisk Område';'Alb Nyremedicinsk område';'Alb Onkologisk Område';'Alb Ortopædkirurgisk Område';'Alb Plastikkirurgisk Område';'Alb Reumatologisk Område';'Alb Urologisk Område';'Alb Vuggeområde';'Alb Øjenområde';'Alb Øre-Næse-Halsområde';'Dro Medicinsk Område';'Hob AMA';'Hob Medicinsk område'};
 
 %% Beregner meanværdi
+mean(data1)
+
+%% variable
 s1=[0 34]
-s2 =[8,8]
+s2 =[8.29,8.29]
 %% plotter figur
 figure;
 bar(data1)
@@ -19,7 +22,7 @@ plot(s1,s2,'r')
 set(gca, 'XTickLabel',str, 'XTick',1:numel(str))
 set(gca,'XTickLabelRotation',45)
 title('Gennemsnitlig overbelægning på Aalborg Universitetshospitals afdelinger')
-ylabel('Antal dage i overbelægning')
+ylabel('Antal dage')
 ylim([0 25])
 set(gca, 'YTick', [0:1:25])
 xlim([0 34])
